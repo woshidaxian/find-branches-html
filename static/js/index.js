1fcd8527784ed2e98ac8835bd007111bbfaf0df2
hwg <wghuangzj@sina.com>
2022-07-03 14:13:52 +0800

    end

--- static/js/index.js
+++ static/js/index.js
@@ -4,9 +4,12 @@ window.onload = function(){
   var oLoadBox = document.getElementsByClassName('load-box')[0]
   var oProgress = document.getElementsByClassName('load-nei')[0]
   var oLoadValue = document.getElementsByClassName('load-text')[0]
+  var oResultBox = document.getElementsByClassName('result-box')[0]
   const baseUrl = 'http://localhost:2022/tool/project'
   let key = null
-
+  let loadVal = 0
+  let timer = null
+  let hasData = false
 
 
   // 点击搜索
@@ -15,12 +18,31 @@ window.onload = function(){
     if(key==""||key==null||key==undefined){
       alert('请输入关键词查找!!!')
     }else{
+      oResultBox.innerHTML = '<div class="no-data">暂无结果</div>'
+      document.getElementsByClassName('no-data')[0].style.display = 'block'
+      oResultBox.style.display = 'none'
+      hasData = false
       axios.get(baseUrl + '/projectList', { params: { pageSize: 100, pageIndex: 1, projectName: ''}}).then(res=>{
         res.data.data.Data.ProjectList.forEach(item => getDepot(item.Id, item.DisplayName))
       }).catch(e=>{
         alert(e)
       })
+      loadVal = 0
       oLoadBox.style.display = 'flex'
+      timer = setInterval(()=>{
+        oLoadValue.innerHTML = loadVal + '%'
+        oProgress.style.width = loadVal + '%'
+        loadVal = loadVal + Math.floor(Math.random() * 5)
+        if(loadVal>=100){
+          clearInterval(timer)
+          loadVal = 100
+          oLoadValue.innerHTML = loadVal + '%'
+          oProgress.style.width = loadVal + '%'
+          oResultBox.style.display = 'block'
+          oLoadBox.style.display = 'none'
+          !hasData ? '' : document.getElementsByClassName('no-data')[0].style.display = 'none'
+        }
+      },50)
     }
   }
 
@@ -35,10 +57,36 @@ window.onload = function(){
 
   // 查仓库分支
   function getBranchs(projectName, depotName, depotId){
-    axios.get(baseUrl + '/getBranchInfo', { params: { depotId: depotId, keyWord:  key} }).then(res => {
-      // res.data.data.Data.projectList
+    axios.get(baseUrl + '/getBranchInfo', { params: { depotId: depotId, keyWord: key, pageIndex: 1, pageSize: 200} }).then(res => {
+      if (res.data.data.Branches.length!=0){
+        hasData = true
+        res.data.data.Branches.forEach(item=>{
+          oResultBox.appendHTML(`
+            <div class="result-line">
+              <div>项目名：<span>${projectName}</span></div>
+              <div>仓库名：<span>${depotName}</span></div>
+              <div>分支名：<span>${item.BranchName.replaceAll(key, `<span class="key-color">${key}</span>`)}</span></div>
+            </div>
+          `)
+        })
+      }
     }).catch(e => {
       alert(e)
     })
   }
+
+  HTMLElement.prototype.appendHTML = function (html) {
+    var divTemp = document.createElement("div"), nodes = null
+      // 文档片段，一次性append，提高性能
+      , fragment = document.createDocumentFragment();
+    divTemp.innerHTML = html;
+    nodes = divTemp.childNodes;
+    for (var i = 0, length = nodes.length; i < length; i += 1) {
+      fragment.appendChild(nodes[i].cloneNode(true));
+    }
+    this.appendChild(fragment);
+    // 据说下面这样子世界会更清净
+    nodes = null;
+    fragment = null;
+  };
 }
